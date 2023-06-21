e431e219a7e77647e2a58076596a04c6731e63a5
Степан Черныx <stepanchernykh@mail.ru>
2023-06-21 17:25:57 +0800

    Added form AddPostForms in forms.py. Added SlugField in models Cars and Brands. Added ShowPost and ShowBrand functions

--- cars/models.py
+++ cars/models.py
@@ -3,13 +3,14 @@ from django.urls import reverse
 
 class Cars(models.Model):
     title = models.CharField(max_length=255, verbose_name="Заголовок")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
     content = models.TextField(blank=True, verbose_name="Текст статьи")
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
     time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
     time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
     is_published = models.BooleanField(default=True, verbose_name="Публикация")
-    brand = models.ForeignKey('Brands', on_delete=models.PROTECT, null=True)
-    characteristics = models.ForeignKey('Characteristics', on_delete=models.PROTECT, null=True)
+    brand = models.ForeignKey('Brands', on_delete=models.PROTECT, verbose_name="Фирмы")
+    characteristics = models.ForeignKey('Characteristics', on_delete=models.PROTECT, verbose_name="Характеристики")
 
     def __str__(self):
         return self.title
@@ -19,6 +20,7 @@ class Cars(models.Model):
     
 class Brands(models.Model):
     name = models.CharField(max_length=30, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
 
     def __str__(self):
         return self.name
