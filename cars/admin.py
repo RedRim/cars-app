5e8831673b6378d8029de925744e6cd64ab41565
pleiada <dreamsobenatic00@mail.ru>
2023-06-23 14:29:52 +0800

    Auto-generated slug has been added

--- cars/admin.py
+++ cars/admin.py
@@ -6,11 +6,13 @@ class CarsAdmin(admin.ModelAdmin):
   list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
   list_display_links = ('id', 'title')
   search_fields = ('title', 'content')
+  prepopulated_fields = {"slug": ('title',)}
 
 class BrandAdmin(admin.ModelAdmin):
   list_display = ('id', 'name')
   list_display_links = ('id', 'name')
   search_fields = ('name',)
-
+  prepopulated_fields = {"slug": ('name',)}
+  
 admin.site.register(Cars, CarsAdmin)
 admin.site.register(Brands, BrandAdmin)
