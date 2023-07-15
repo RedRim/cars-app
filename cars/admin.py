04f23c7da1d0745d26f60aacf9ee37da5cedbc56
Pavel <dreamsobenatic00@mail.ru>
2023-07-15 16:23:49 +0800

    The choice of the author is now realized by raw-id-field

--- cars/admin.py
+++ cars/admin.py
@@ -2,33 +2,35 @@ from django.contrib import admin
 
 from .models import *
 
-class PostsAdmin(admin.ModelAdmin):
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
   list_display = ('id', 'title', 'time_create', 'photo', 'author', 'is_published')
   list_display_links = ('id', 'title')
   search_fields = ('title', 'content')
   prepopulated_fields = {"slug": ('title',)}
+  #Выбор автора не из списка, а по id (с возможностью поиска)
+  raw_id_fields = ['author']
 
+@admin.register(Brands)
 class BrandAdmin(admin.ModelAdmin):
   list_display = ('id', 'name')
   list_display_links = ('id', 'name')
   search_fields = ('name',)
   prepopulated_fields = {"slug": ('name',)}
 
+@admin.register(FeedbackMessage)
 class FeedbackMessageAdmin(admin.ModelAdmin):
   list_display = ('id', 'author', 'short_content', 'content')
   list_display_links = ('id', 'author')
   search_fields = ('short_conent', 'auhtor')
 
-class ComentsAdmin(admin.ModelAdmin):
+@admin.register(Comment)
+class ComentAdmin(admin.ModelAdmin):
   list_display = ('id', 'time_create', 'content')
 
+@admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
   list_display = ('slug', 'first_name', 'last_name', 'is_moder')
   list_display_links = ('slug',)
   search_fields = ('login', 'first_name', 'last_name')
-  
-admin.site.register(Post, PostsAdmin)
-admin.site.register(Brands, BrandAdmin)
-admin.site.register(CustomUser, CustomUserAdmin)
-admin.site.register(FeedbackMessage, FeedbackMessageAdmin)
-admin.site.register(Comment, ComentsAdmin)
+
