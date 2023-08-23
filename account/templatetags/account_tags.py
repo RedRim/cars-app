b6dfd8e284159b5b9d8225e9c50330c90aa7df17
Pavel <dreamsobenatic00@mail.ru>
2023-08-23 12:44:35 +0800

    Subscribe button template changed

--- account/templatetags/account_tags.py
+++ account/templatetags/account_tags.py
@@ -3,6 +3,6 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('account/includes/subscribe_button.html')
-def subscribe_button(user_to):
-    return {'user': user_to}
+def subscribe_button(request, user_to):
+    return {'request': request, 'user': user_to}
 
